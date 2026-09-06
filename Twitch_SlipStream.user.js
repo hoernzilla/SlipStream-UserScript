29c3607e54d7c74a788f4b664ec7c50226222fa3
hoernzilla <hoernzilla@gmail.com>
2026-09-06 10:26:18 +0200

    Twitch_SlipStream.user.js

--- Twitch_SlipStream.user.js
+++ Twitch_SlipStream.user.js
@@ -5,7 +5,7 @@
 // @description  Mehrere Lösungen zum Blockieren von Twitch-Werbung
 // @updateURL    https://github.com/hoernzilla/Twitch-SlipStream/raw/main/Twitch_SlipStream.user.js
 // @downloadURL  https://github.com/hoernzilla/Twitch-SlipStream/raw/main/Twitch_SlipStream.user.js
-// @author       https://github.com/cleanlock/VideoAdBlockForTwitch#credits
+// @author       BjOOgle
 // @match        *://*.twitch.tv/*
 // @run-at       document-start
 // @grant        none
