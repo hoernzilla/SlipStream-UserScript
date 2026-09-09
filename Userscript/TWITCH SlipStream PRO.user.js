0e718ddad86e5648cb0f256af5be1a9f85107fce
hoernzilla <hoernzilla@gmail.com>
2026-09-09 02:21:31 +0200

    Update TWITCH SlipStream PRO.user.js

--- Userscript/TWITCH SlipStream PRO.user.js
+++ Userscript/TWITCH SlipStream PRO.user.js
@@ -4,8 +4,8 @@
 // @version      32.21.5
 // @description  "Twitch AD-Blocker" & "SUB-Only VOD Bypass"
 // @author       BjOOgle
-// @updateURL    https://github.com/hoernzilla/SlipStream-UserScript/blob/main/Userscript/TWITCH%20SlipStream%20PRO-32.10.1.user.js
-// @downloadURL  https://github.com/hoernzilla/SlipStream-UserScript/blob/main/Userscript/TWITCH%20SlipStream%20PRO-32.10.1.user.js
+// @updateURL    https://github.com/hoernzilla/SlipStream-UserScript/blob/main/Userscript/TWITCH%20SlipStream%20PRO.user.js
+// @downloadURL  https://github.com/hoernzilla/SlipStream-UserScript/blob/main/Userscript/TWITCH%20SlipStream%20PRO.user.js
 // @match        *://*.twitch.tv/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/hls.js/1.6.15/hls.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/plyr/3.8.4/plyr.min.js
